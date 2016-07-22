7ed2c5c1133f20141aea147bde96de782f288a44
Armando113 <armandogarcin113@gmail.com>
2016-07-22 14:25:45 -0500

    Copied the folder

---
+++ BDDataStructures/BDDataStructures/DoubleLinkedList/DLLIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class DLLIterator<T>
+{
+    DLLNode<T> myNode;
+
+    public DLLIterator()
+    {
+
+    }
+
+    //Set the starting Node
+    public void SetStartNode(DLLNode<T> _node)
+    {
+        myNode = _node;
+    }
+
+    //Walk to the next node, if available
+    public void GoNext()
+    {
+        if (myNode != null)
+        {
+            myNode = myNode.GetNext();
+        }
+    }
+
+    //Walk to the previous node if available
+    public void GoPrev()
+    {
+        if (myNode != null)
+        {
+            myNode = myNode.GetPrev();
+        }
+    }
+
+    //Get the node you are currently in
+    public DLLNode<T> GetNode()
+    {
+        return myNode;
+    }
+}
---
+++ BDDataStructures/BDDataStructures/DoubleLinkedList/DLLNode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class DLLNode<T>
+{
+    //Our links to our neighbour nodes
+    private DLLNode<T> pNext;
+    private DLLNode<T> pPrev;
+
+    //The data we are to hold
+    private T pData;
+
+    //Simple constructor
+    public DLLNode()
+    {
+        //Set the neighbour links to null
+        pNext = null;
+        pPrev = null;
+        //Data is null
+        pData = default(T);
+    }
+    public DLLNode(T _data)
+    {
+        //Set the neighbour links to null
+        pNext = null;
+        pPrev = null;
+        //Set data to the given one
+        pData = _data;
+    }
+
+
+    //The pNext functions
+    public void SetNext(DLLNode<T> _next)
+    {
+        pNext = _next;
+    }
+    public DLLNode<T> GetNext()
+    {
+        return pNext;
+    }
+
+    //The pPrev functions
+    public void SetPrev(DLLNode<T> _prev)
+    {
+        pPrev = _prev;
+    }
+    public DLLNode<T> GetPrev()
+    {
+        return pPrev;
+    }
+
+    //Data functions
+    public void SetData(T _data)
+    {
+        pData = _data;
+    }
+    public T GetData()
+    {
+        return pData;
+    }
+}
